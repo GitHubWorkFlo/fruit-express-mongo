1428ce00e49ed55b57b7541be4ad622afa09b8be
yimd85 <yimd85@gmail.com>
2022-12-03 16:51:57 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
-require("dotenv").config()  // Load env variables
-const express = require('express') // bring in express to make our app
-const morgan = require('morgan') // nice logger for our request
-const methodOverride = require('method-override') // allows us to override post request from our ejs/forms
-const PORT = process.env.PORT
-const FruitRouter = require('./controllers/fruit')
+require("dotenv").config(); // Load env variables
+const express = require("express"); // bring in express to make our app
+const morgan = require("morgan"); // nice logger for our request
+const methodOverride = require("method-override"); // allows us to override post request from our ejs/forms
+const PORT = process.env.PORT;
+const FruitRouter = require("./controllers/fruit");
+const UserRouter = require("./controllers/user");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-const app = express()
+const app = express();
 
 //////////////////////////////////////////////
 //////// Middlewares
 ///////////////////////////////////////////////
 
-app.use(morgan('tiny'))
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(morgan("tiny"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+        saveUninitialized: true,
+        resave: false,
+    })
+);
 
 // app.get('/', homeRoutes)
 // app.get('/store', storeRoutes)
 // app.get('/user', userRoutes)
-app.use('/fruits', FruitRouter)
+app.use("/fruits", FruitRouter);
+app.use("/user", UserRouter);
 
-app.listen(PORT, ()=> console.log(`Who let the dogs out on port: ${PORT}`))
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(PORT, () => console.log(`Who let the dogs out on port: ${PORT}`));
